d6e510497d9a16fa8e45f71908c21271566f63df
Jamie Thompson <bishbashboshjt@gmail.com>
2023-04-29 23:30:37 +0200

    add swift package

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterScala",
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
+    products: [
+        .library(name: "TreeSitterScala", targets: ["TreeSitterScala"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterScala",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "corpus",
+                    "examples",
+                    "grammar.js",
+                    "LICENSE",
+                    "package.json",
+                    "README.md",
+                    "src/grammar.json",
+                    "src/node-types.json",
+                ],
+                sources: [
+                    "src/parser.c",
+                    "src/scanner.c",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
+)
